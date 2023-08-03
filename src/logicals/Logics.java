e002f9dcc59fc0ae42e22edb608f49606e23599f
dhaya1407 <dhayanithi1407@gmail.com>
2023-08-03 13:26:51 +0530

    update

---
+++ src/logicals/Logics.java
@@ -0,0 +1,20 @@
+package logicals;
+
+
+
+public class Logics {
+
+
+	
+	public static void main(String[] args) {
+	
+		
+		for(int i=1;i<=15;i++) {
+			System.out.println(i);
+			
+		}
+		
+		
+	}	
+	}
+
